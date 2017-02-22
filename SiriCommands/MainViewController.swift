124c3c5a352b49fae15d1faa4f6e19ed491fffa6
Diogo Ribeiro de Oliveira <diogoro.dba@gmail.com>
2017-02-21 22:01:41 -0300

    Add UIBarButton to present Setting controllers and add some functions to use UITableView

--- SiriCommands/MainViewController.swift
+++ SiriCommands/MainViewController.swift
@@ -8,10 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        self.tableView.tableFooterView = UIView()
+        
+        let barItemButton = UIBarButtonItem(image: UIImage(named: "settings"), style: UIBarButtonItemStyle.plain, target: self, action: #selector(self.chamaNois))
+        self.navigationItem.rightBarButtonItem = barItemButton
+        
+        //self.readJson()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +29,74 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func chamaNois () {
+        self.performSegue(withIdentifier: "settings", sender: nil)
+    }
+    
+    func readJson() {
+        do {
+            if let file = Bundle.main.url(forResource: "Hey-Siri", withExtension: "json") {
+                let data = try Data(contentsOf: file)
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                if let object = json as? [String:Any] {
+                    print(object)
+                } else if let object = json as? [Any] {
+                    print(object)
+                } else {
+                    print("JSON is invalid")
+                }
+            } else {
+                print("No file")
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    //MARK - TableViewDatasource
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+//    func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+//        return ["Section 0","Section 1"]
+//    }
+//    
+//    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        if section == 0 {
+//            return "Section 0"
+//        } else if section == 1 {
+//            return "Section 1"
+//        } else {
+//            return ""
+//        }
+//    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Catch number of languanges from json
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        
+        return cell
+    }
+    
+    //MARK - TableViewDelegate
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "settings" {
+            //let destinantion = segue.destination
+        }
+    }
+    
 }
 
