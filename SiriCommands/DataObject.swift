736a6b71f16f595d036bc2c647dde0e80184fa95
Diogo Ribeiro de Oliveira <diogoro.dba@gmail.com>
2017-02-21 21:51:33 -0300

    Add portuguese language

---
+++ SiriCommands/DataObject.swift
@@ -0,0 +1,9 @@
+//
+//  DataObject.swift
+//  SiriCommands
+//
+//  Created by Diogo Ribeiro de Oliveira on 21/02/17.
+//  Copyright © 2017 Diogo Ribeiro de Oliveira. All rights reserved.
+//
+
+import Foundation
